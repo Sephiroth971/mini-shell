e031ac03510364ddbea41cfa820d7a68c3a60e4b
sephiroth <avivies971@gmail.com>
2026-03-12 21:04:20 +0100

    ajout d'un tableau contenant les token

--- shell/main.c
+++ shell/main.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-void tokenize(char *line){
+char **tokenize(char *line, char **argvs){
     char *token = strtok(line," \t");
+    int i=0;
     while(token != NULL){
-        printf("%s ", token);
+        argvs[i] = token;
+        i++;
         token = strtok(NULL," \t");
     }
+    return argvs;
 }
 void read_line(char *line){
-        fgets(line,256,stdin);
+    fgets(line,256,stdin);
     size_t len = strlen(line);
     if (line[len - 1]=='\n'){
         line[len - 1]='\0';
     }
 }
+void execute(char *line){
 
+}
 int main(){
     char line[256];
+    char *argvs[20];
     printf("shell>>");
     read_line(line);
-    tokenize(line);
+    tokenize(line,argvs);
+    for (int i = 0; argvs[i] != NULL; i++) {
+    printf("argvs[%d] = %s\n", i, argvs[i]);
+}
+
     
 }
