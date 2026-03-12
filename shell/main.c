c7f0d6b81f43c7878c6a8a9be67ea945b9afaeec
sephiroth <avivies971@gmail.com>
2026-03-12 20:43:47 +0100

    factorisation

--- shell/main.c
+++ shell/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    char line[256];
-    printf("shell>>");
-    fgets(line,256,stdin);
-    size_t len = strlen(line);
-    if (line[len - 1]=='\n'){
-        line[len - 1]='\0';
-    }
+
+void tokenize(char *line){
     char *token = strtok(line," \t");
     while(token != NULL){
         printf("%s ", token);
         token = strtok(NULL," \t");
     }
+}
+void read_line(char *line){
+        fgets(line,256,stdin);
+    size_t len = strlen(line);
+    if (line[len - 1]=='\n'){
+        line[len - 1]='\0';
+    }
+}
+
+int main(){
+    char line[256];
+    printf("shell>>");
+    read_line(line);
+    tokenize(line);
     
 }
